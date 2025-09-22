cca0f1ac3be0bb00a18fc22634c16cc09bac0353
DominikZurawski <38438473+DominikZurawski@users.noreply.github.com>
2025-09-22 23:19:06 +0200

    Added CAN custom code

---
+++ Core/Src/main.c
@@ -0,0 +1,235 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stm32l4xx_hal_can.h>
+
+/* Private function prototypes -----------------------------------------------*/
+void GPIO_Init(void);
+void Error_handler(void);
+void UART2_Init(void);
+void SystemClock_Config(uint8_t);
+void CAN1_Init(void);
+void CAN1_Tx(void);
+void CAN1_Rx(void);
+void CAN_Filter_Config(void);
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+CAN_HandleTypeDef hcan1;
+
+int main(void)
+{
+	HAL_Init();
+	SystemClock_Config(SYS_CLOCK_FREQ_50_MHZ);
+	GPIO_Init();
+	UART2_Init();
+	CAN1_Init();
+	CAN_Filter_Config();
+
+	if( HAL_CAN_Start(&hcan1) != HAL_OK)
+	{
+		Error_handler();
+	}
+
+	CAN1_Tx();
+	CAN1_Rx();
+
+	while(1);
+
+	return 0;
+}
+
+void SystemClock_Config(uint8_t clock_freq )
+{
+	RCC_OscInitTypeDef osc_init;
+	RCC_ClkInitTypeDef clk_init;
+
+	uint32_t FLatency =0;
+
+	osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	osc_init.HSIState = RCC_HSI_ON;
+	osc_init.HSICalibrationValue = 16;
+	osc_init.PLL.PLLState = RCC_PLL_ON;
+	osc_init.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+
+	switch(clock_freq) {
+        case SYS_CLOCK_FREQ_50_MHZ:{
+            osc_init.PLL.PLLM = 4;
+            osc_init.PLL.PLLN = 25;
+            osc_init.PLL.PLLR=2;
+            osc_init.PLL.PLLP = 2;
+            osc_init.PLL.PLLQ  = 2;
+
+            clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+                                RCC_CLOCKTYPE_PCLK1  | RCC_CLOCKTYPE_PCLK2;
+            clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+            clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+            clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+            clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+            FLatency = FLASH_ACR_LATENCY_1WS;
+            break;
+    }
+    case SYS_CLOCK_FREQ_80_MHZ: {
+        osc_init.PLL.PLLM = 2;
+        osc_init.PLL.PLLN = 20;
+        osc_init.PLL.PLLR=2;
+        osc_init.PLL.PLLP = 2;
+        osc_init.PLL.PLLQ  = 2;
+
+        clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+                            RCC_CLOCKTYPE_PCLK1  | RCC_CLOCKTYPE_PCLK2;
+        clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+        clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+        clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+        clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+        FLatency = FLASH_ACR_LATENCY_2WS;
+
+        break;
+    }
+
+	default:
+	  return;
+	}
+
+	if(HAL_RCC_OscConfig(&osc_init) != HAL_OK)
+	{
+		Error_handler();
+	}
+
+
+	if(HAL_RCC_ClockConfig(&clk_init,FLatency) != HAL_OK)
+	{
+		Error_handler();
+	}
+
+	//Systick configuration
+
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+}
+
+void CAN1_Tx(void)
+{
+	char msg[50];
+
+	CAN_TxHeaderTypeDef TxHeader;
+
+	uint32_t TxMailbox;
+
+	uint8_t our_message[5] = {'H','E','L','L','O'};
+
+	TxHeader.DLC = 5;
+	TxHeader.StdId = 0x65D;
+	TxHeader.IDE   = CAN_ID_STD;
+	TxHeader.RTR = CAN_RTR_DATA;
+
+	if( HAL_CAN_AddTxMessage(&hcan1,&TxHeader,our_message,&TxMailbox) != HAL_OK)
+	{
+		Error_handler();
+	}
+
+	while( HAL_CAN_IsTxMessagePending(&hcan1,TxMailbox));
+
+	sprintf(msg,"Message Transmitted\r\n");
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
+}
+
+void CAN1_Rx(void)
+{
+	CAN_RxHeaderTypeDef RxHeader;
+	uint8_t rcvd_msg[5];
+
+	char msg[50];
+
+	//we are waiting for at least one message in to the RX FIFO0
+	while(! HAL_CAN_GetRxFifoFillLevel(&hcan1,CAN_RX_FIFO0));
+
+	if(HAL_CAN_GetRxMessage(&hcan1,CAN_RX_FIFO0,&RxHeader,rcvd_msg) != HAL_OK)
+	{
+		Error_handler();
+	}
+
+	sprintf(msg,"Message Received : %s\r\n",rcvd_msg);
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
+}
+
+void CAN_Filter_Config(void)
+{
+	CAN_FilterTypeDef can1_filter_init;
+
+	can1_filter_init.FilterActivation = ENABLE;
+	can1_filter_init.FilterBank  = 0;
+	can1_filter_init.FilterFIFOAssignment = CAN_RX_FIFO0;
+	can1_filter_init.FilterIdHigh = 0x0000;
+	can1_filter_init.FilterIdLow = 0x0000;
+	can1_filter_init.FilterMaskIdHigh = 0x0000;
+	can1_filter_init.FilterMaskIdLow = 0x0000;
+	can1_filter_init.FilterMode = CAN_FILTERMODE_IDMASK;
+	can1_filter_init.FilterScale = CAN_FILTERSCALE_32BIT;
+
+	if( HAL_CAN_ConfigFilter(&hcan1,&can1_filter_init) != HAL_OK)
+	{
+		Error_handler();
+	}
+}
+
+
+void GPIO_Init(void)
+{
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+	GPIO_InitTypeDef ledgpio;
+	ledgpio.Pin = GPIO_PIN_5;
+	ledgpio.Mode = GPIO_MODE_OUTPUT_PP;
+	ledgpio.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA,&ledgpio);
+}
+
+
+void UART2_Init(void)
+{
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	if ( HAL_UART_Init(&huart2) != HAL_OK )
+	{
+		//There is a problem
+		Error_handler();
+	}
+}
+
+
+void CAN1_Init(void)
+{
+	hcan1.Instance = CAN1;
+	hcan1.Init.Mode = CAN_MODE_LOOPBACK;
+	hcan1.Init.AutoBusOff = ENABLE;
+	hcan1.Init.AutoRetransmission = ENABLE;
+	hcan1.Init.AutoWakeUp = DISABLE;
+	hcan1.Init.ReceiveFifoLocked = DISABLE;
+	hcan1.Init.TimeTriggeredMode = DISABLE;
+	hcan1.Init.TransmitFifoPriority = DISABLE;
+
+	//Settings related to CAN bit timings
+	hcan1.Init.Prescaler = 5;
+	hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
+	hcan1.Init.TimeSeg1 = CAN_BS1_8TQ;
+	hcan1.Init.TimeSeg2 = CAN_BS2_1TQ;
+
+	if ( HAL_CAN_Init (&hcan1) != HAL_OK)
+	{
+		Error_handler();
+	}
+}
+
+void Error_handler(void)
+{
+	while(1);
+}
+
